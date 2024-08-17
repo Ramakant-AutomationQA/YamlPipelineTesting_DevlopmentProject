f16ea8a8222b7e3eaf635f891078a903fba67b24
dell <dell@DESKTOP-IJIHQ5I>
2024-08-17 23:29:34 +0530

    dev01

---
+++ YamlPipelineTesting_DevlopmentProject/src/main/java/package01/ClassDevelopment_A.java
@@ -0,0 +1,9 @@
+package package01;
+
+public class ClassDevelopment_A {
+
+	public static void main(String[] args) {
+		System.out.println("ClassDevelopment_A====Simple_Development_Project01====01");
+	}
+
+}
