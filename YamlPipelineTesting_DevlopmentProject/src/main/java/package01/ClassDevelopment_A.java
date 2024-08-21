c356e8f65e5218a2c72fca4647629eef5df99301
dell <dell@DESKTOP-IJIHQ5I>
2024-08-22 00:50:25 +0530

    22Aug2024_Commit_01

--- YamlPipelineTesting_DevlopmentProject/src/main/java/package01/ClassDevelopment_A.java
+++ YamlPipelineTesting_DevlopmentProject/src/main/java/package01/ClassDevelopment_A.java
@@ -3,7 +3,7 @@ package package01;
 public class ClassDevelopment_A {
 
 	public static void main(String[] args) {
-		System.out.println("ClassDevelopment_A====Simple_Development_Project01====02");
+		System.out.println("ClassDevelopment_A====Simple_Development_Project01====03");
 	}
 
 }
